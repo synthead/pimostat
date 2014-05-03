79fc72b7c655bc36de39e40e9fb2976f4ff50486
Maxwell Pray <synthead@gmail.com>
2014-05-02 20:08:10 -0700

    Added Relay, Sensor, and Temperature models.

--- web_client/thermostat/models.py
+++ web_client/thermostat/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Relay(models.Model):
+  channel = models.IntegerField(unique=True)
+  state = models.NullBooleanField()
+  enabled = models.BooleanField(default=False)
+
+
+class Sensor(models.Model):
+  serial = models.CharField(max_length=255, unique=True)
+  temperature = models.DecimalField(max_digits=6, decimal_places=3, null=True)
+  enabled = models.BooleanField(default=False)
+
+
+class Thermostat(models.Model):
+  relay = models.ForeignKey(Relay)
+  sensor = models.ForeignKey(Sensor)
+  desired_temperature = models.DecimalField(
+      max_digits=6, decimal_places=3, null=True)
+  lower_deviation = models.DecimalField(
+      max_digits=6, decimal_places=3, default=0.5)
+  upper_deviation = models.DecimalField(
+      max_digits=6, decimal_places=3, default=0.5)
+  enabled = models.BooleanField(default=False)
