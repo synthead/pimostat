0cb758144ca1ee8e09470bf635a5c1a11bed7c0d
Maxwell Pray <synthead@gmail.com>
2014-06-02 16:54:48 -0700

    Thermostat properties on page update immediately after they're updated.

--- pimostat/templates/index.js
+++ pimostat/templates/index.js
@@ -1,29 +1,35 @@
 $(".thermostat-submit").click(function() {
   var button = this;
+  var pk = this.id.replace(/submit-/, "");
 
   $.ajax({
     type: "POST",
     url: "/update_thermostat",
     data: $(button.parentNode).serialize(),
     beforeSend: function() {
+      clearInterval(pollers[pk]);
       $(button).prop("disabled", true);
     }
   }).done(function() {
+    pollThermostat(pk);
     $(button).prop("disabled", false);
   });
 });
 
 var update_frequency = {{ update_frequency }} * 1000;
 
-{% for form in thermostat_formset %}
-  setTimeout(pollThermostat, update_frequency, {{ form.instance.pk }});
-{% endfor %}
+var pollers = {
+    {% for form in thermostat_formset %}
+      {{ form.instance.pk}}: setTimeout(
+          pollThermostat, update_frequency, {{ form.instance.pk }}),
+    {% endfor %}
+};
 
 function pollThermostat(pk) {
+  pollers[pk] = setTimeout(pollThermostat, update_frequency, pk);
+
   $.getJSON("/poll_thermostat/" + pk, function(response) {
     $("#temperature-" +pk).text(response.temperature);
     $("#actuated-" + pk).text((response.actuated ? "True" : "False"));
   });
-
-  setTimeout(pollThermostat, update_frequency, pk);
 }
